1722c4aae4ec7cee7683ec434b88a86fab83fdc2
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:38:34 +0200

    create third line

--- src/main/java/week1/Topplista.java
+++ src/main/java/week1/Topplista.java
@@ -7,6 +7,8 @@ public class Topplista {
         //Rubrik på första raden, sedan en rad per post.
         System.out.println("Mina favorite barn program: ");
         System.out.println("1 - Bluey");
+        System.out.println("2 - Spidey amazing friends");
+
 
     }
 }
