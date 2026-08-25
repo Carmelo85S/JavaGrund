1206f5958586b20396cf63aec8c98e7a70ebbbc9
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 12:02:04 +0200

    created VisitKort class, commented details exercise

---
+++ src/main/java/week1/VisitKort.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class VisitKort {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut ett enkelt visitkort:
+        // en ram runt om, och inuti ditt namn, din framtida yrkesroll
+        //och din GitHub-adress, snyggt placerade.
+    }
+}
