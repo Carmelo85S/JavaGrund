2bf0e45f80d39b9aed0490e6a1ba70882f4eb2c4
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 11:56:24 +0200

    added top line

--- src/main/java/week1/HelloWorldNew.java
+++ src/main/java/week1/HelloWorldNew.java
@@ -5,5 +5,6 @@ public class HelloWorldNew {
         //Skriv ett program som skriver ut en kort text inramad
         //i en låda byggd av tecknen +, - och |. Lådan ska sluta tätt
         //runt texten och se rak ut.
+        System.out.println("+--------------+");
     }
 }
