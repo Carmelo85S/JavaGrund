7e785620bb430d58a711aac7cf07080495701c5c
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 12:10:26 +0200

    added 3 item

--- src/main/java/week1/Kvitto.java
+++ src/main/java/week1/Kvitto.java
@@ -5,10 +5,12 @@ public class Kvitto {
         //Skriv ett program som skriver ut ett litet kvitto:
         // en rubrik, tre varor med pris, en avdelarrad, och en summarad.
         // Få priserna att stå i en rak kolumn genom att fylla ut med blanksteg — det är klurigare än det låter.
-        System.out.println("KVITTO - Konsult AB");
-        System.out.println("-------------------");
-        System.out.println("Front end    2000kr");
-        System.out.println("Back end     6000kr");
+        System.out.println("KVITTO - Konsult AB   ");
+        System.out.println("----------------------");
+        System.out.println("Front end       2000kr");
+        System.out.println("Back end        6000kr");
+        System.out.println("Data Tekniker    600kr");
+
 
     }
 }
