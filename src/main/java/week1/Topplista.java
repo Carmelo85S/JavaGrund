ca50ac9c1fcdd77ff923c55c6e3a3068934365de
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:36:00 +0200

    create class Topplista and commented details

---
+++ src/main/java/week1/Topplista.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Topplista {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut en numrerad topp-3-lista över något du gillar
+        //(spel, mat, musik — du väljer).
+        //Rubrik på första raden, sedan en rad per post.
+
+    }
+}
