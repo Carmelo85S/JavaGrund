09df4012a7c0db89a21f788e1b6daeac428e1eba
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:14:38 +0200

    create exercise HalsningNamn and commetend details ex

---
+++ src/main/java/week1/HalsningNamn.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class HalsningNamn {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut tre rader:
+        // en hälsning, ditt namn, och dagens datum
+        // (skriv datumet rakt in texten — vi använder inte riktiga datumtyper än).
+    }
+}
