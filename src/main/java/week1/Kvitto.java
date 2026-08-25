fdbe483c7640cceb30a10a56a87d19127feeb39d
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 12:07:02 +0200

    created kvitto class and added details exercise in comment

---
+++ src/main/java/week1/Kvitto.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Kvitto {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut ett litet kvitto:
+        // en rubrik, tre varor med pris, en avdelarrad, och en summarad.
+        // Få priserna att stå i en rak kolumn genom att fylla ut med blanksteg — det är klurigare än det låter.
+    }
+}
