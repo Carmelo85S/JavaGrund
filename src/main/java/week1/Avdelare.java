2e171a34091bcce13e33080813cfa6caf56044ed
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:52:57 +0200

    create new class for exrcise Avledare, added a comments with ex details

---
+++ src/main/java/week1/Avdelare.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Avdelare {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut
+        // en rubrik, en avdelarrad av streck precis under, och en mening därunder.
+        //Avdelaren ska vara ungefär lika lång som rubriken.
+    }
+}
