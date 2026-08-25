cb7c842e2a08bee813b7a767a520752273081cdc
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:19:51 +0200

    commented ex details

--- src/main/java/week1/Presentation.java
+++ src/main/java/week1/Presentation.java
@@ -2,6 +2,8 @@ package week1;
 
 public class Presentation {
     public static void main(String[] args) {
-
+        //Skriv ett program som presenterar dig själv på fyra rader:
+        // namn, ort, varför du valt utbildningen,
+        // och en kul fakt om dig.
     }
 }
