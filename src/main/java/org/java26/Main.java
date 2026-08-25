48eb7a4a3c71af51e5016a79d68621f25d6dbdac
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:05:32 +0200

    first commit

--- src/main/java/org/java26/Main.java
+++ src/main/java/org/java26/Main.java
@@ -4,8 +4,6 @@ package org.java26;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
     }
 }
