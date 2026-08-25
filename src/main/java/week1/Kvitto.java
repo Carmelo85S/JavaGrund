f7eeaae3b1778af438b61216b6cdf3158f9e88e6
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 12:07:51 +0200

    added rubrik

--- src/main/java/week1/Kvitto.java
+++ src/main/java/week1/Kvitto.java
@@ -5,5 +5,7 @@ public class Kvitto {
         //Skriv ett program som skriver ut ett litet kvitto:
         // en rubrik, tre varor med pris, en avdelarrad, och en summarad.
         // Få priserna att stå i en rak kolumn genom att fylla ut med blanksteg — det är klurigare än det låter.
+        System.out.println("KVITTO - Konsult AB");
+
     }
 }
