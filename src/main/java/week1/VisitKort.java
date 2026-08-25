a7a6781e8fc71e28cd7ca91d38f7f022cb3b36e4
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 12:04:22 +0200

    added details card

--- src/main/java/week1/VisitKort.java
+++ src/main/java/week1/VisitKort.java
@@ -5,7 +5,9 @@ public class VisitKort {
         //Skriv ett program som skriver ut ett enkelt visitkort:
         // en ram runt om, och inuti ditt namn, din framtida yrkesroll
         //och din GitHub-adress, snyggt placerade.
-        System.out.println("==============================");
-        System.out.println("==============================");
+        System.out.println("====================================================");
+        System.out.println("  Carmelo Salis - Java-utvecklare (under utbildning)");
+        System.out.println("  github.com/carmelo85S");
+        System.out.println("====================================================");
     }
 }
