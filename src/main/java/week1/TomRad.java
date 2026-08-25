4f42680fc42411aa0ecfad44dc032edfd0d5499f
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:59:55 +0200

    create new class for exercise TomRad, added commented details

---
+++ src/main/java/week1/TomRad.java
@@ -0,0 +1,8 @@
+package week1;
+
+public class TomRad {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut två stycken text med exakt en tom rad emellan.
+        // Klura ut hur du får programmet att skriva ut en helt tom rad.
+    }
+}
