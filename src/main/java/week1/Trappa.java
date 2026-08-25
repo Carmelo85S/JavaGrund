04b925907764812b89a0f0f279030b42ce977370
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 11:59:37 +0200

    create new java class exercise Trappa, commented details

---
+++ src/main/java/week1/Trappa.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Trappa {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut en trappa av stjärnor:
+        // fem rader, där varje rad har en stjärna mer än raden ovanför.
+        //(Du känner inte till loopar än — skriv ut varje rad för sig. Tålamod räknas.)
+    }
+}
