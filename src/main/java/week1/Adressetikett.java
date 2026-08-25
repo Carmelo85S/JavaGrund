0e6db1675e7ee480fd21aa0d162565710b99d52c
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:55:42 +0200

    created new class for exercise Adressetikett, add comment with details exercise

---
+++ src/main/java/week1/Adressetikett.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Adressetikett {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut en adressetikett på fyra rader:
+        // namn, gatuadress, postnummer och ort, sam land.
+
+    }
+}
