0564c740bbbb77e8251a448d8f0ea30954d46876
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:36:57 +0200

    create first line

--- src/main/java/week1/Topplista.java
+++ src/main/java/week1/Topplista.java
@@ -5,6 +5,8 @@ public class Topplista {
         //Skriv ett program som skriver ut en numrerad topp-3-lista över något du gillar
         //(spel, mat, musik — du väljer).
         //Rubrik på första raden, sedan en rad per post.
+        System.out.println("Mina favorite barn program: ");
+
 
     }
 }
