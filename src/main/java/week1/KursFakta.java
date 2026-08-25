5eca60bda418c0d846b4632488c06b14e788e577
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:29:54 +0200

    third line

--- src/main/java/week1/KursFakta.java
+++ src/main/java/week1/KursFakta.java
@@ -8,5 +8,6 @@ public class KursFakta {
         // hur många inlämningar det finns, och när inlämning 1 ska in.
         System.out.println("Kurs: Programmering med Java, grund");
         System.out.println("Längd: 8 veckor");
+        System.out.println("Antal inlämningar: 2");
     }
 }
