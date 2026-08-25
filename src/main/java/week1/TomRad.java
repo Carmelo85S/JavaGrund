47465597dc6268b8c54d2436544322df2086cbb0
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 11:00:25 +0200

    first line

--- src/main/java/week1/TomRad.java
+++ src/main/java/week1/TomRad.java
@@ -4,5 +4,6 @@ public class TomRad {
     public static void main(String[] args) {
         //Skriv ett program som skriver ut två stycken text med exakt en tom rad emellan.
         // Klura ut hur du får programmet att skriva ut en helt tom rad.
+        System.out.println("Detta ar forsta stycket.");
     }
 }
