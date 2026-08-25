9536d8566e57a60dc4ec85340c5a187010d9d5d4
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 11:55:30 +0200

    create new classm commented details ex

---
+++ src/main/java/week1/HelloWorldNew.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class HelloWorldNew {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut en kort text inramad
+        //i en låda byggd av tecknen +, - och |. Lådan ska sluta tätt
+        //runt texten och se rak ut.
+    }
+}
