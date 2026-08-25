e35d0dd5ec03b363bf2773d5ae5e8a79fddbb114
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:27:03 +0200

    create class ex KursFakta and commented details exercise

---
+++ src/main/java/week1/KursFakta.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class KursFakta {
+    public static void main(String[] args) {
+        //Skriv ett program som skriver ut fyra fakta om kursen, en per rad:
+        // kursens namn,
+        // hur många veckor den är,
+        // hur många inlämningar det finns, och när inlämning 1 ska in.
+    }
+}
