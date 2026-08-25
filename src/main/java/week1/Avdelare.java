e0ee0bf8c56f6e7e217c826a7285f88016ff609d
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 10:53:23 +0200

    add first line

--- src/main/java/week1/Avdelare.java
+++ src/main/java/week1/Avdelare.java
@@ -5,5 +5,6 @@ public class Avdelare {
         //Skriv ett program som skriver ut
         // en rubrik, en avdelarrad av streck precis under, och en mening därunder.
         //Avdelaren ska vara ungefär lika lång som rubriken.
+        System.out.println("VECKA 1 - KOM IGÅNG");
     }
 }
