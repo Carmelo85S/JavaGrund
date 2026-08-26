3fda7e6e5c1ef23fcb1a1a581b0ce48b9545daff
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 11:57:25 +0200

    input number of seconds

--- src/main/java/week1/Time.java
+++ src/main/java/week1/Time.java
@@ -1,9 +1,14 @@
 package week1;
+import java.util.Scanner;
 
 public class Time {
     public static void main(String[] args) {
         // Fråga efter ett antal sekunder och skriv ut hur lång tid det är,
         // uppdelat i hela timmar, minuter och sekunder.
         //Du behöver både division och rest för att plocka isär talet.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Number of seconds: ");
+        int seconds = input.nextInt();
+
     }
 }
