e833e1e7a40316094e0587a6be55d0067858ef04
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 11:53:33 +0200

    ex time, details

---
+++ src/main/java/week1/Time.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Time {
+    public static void main(String[] args) {
+        // Fråga efter ett antal sekunder och skriv ut hur lång tid det är,
+        // uppdelat i hela timmar, minuter och sekunder.
+        //Du behöver både division och rest för att plocka isär talet.
+    }
+}
