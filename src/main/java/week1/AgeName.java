d351f223e524348325091a19695dff1ed00c0612
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 11:46:34 +0200

    import, declared scanner

--- src/main/java/week1/AgeName.java
+++ src/main/java/week1/AgeName.java
@@ -1,11 +1,15 @@
 package week1;
 
+import java.util.Scanner;
+
 public class AgeName {
     public static void main(String[] args) {
         //Fråga först efter användarens ålder (ett heltal) och sedan efter hela namnet
         // (för- och efternamn på samma rad). Skriv till sist ut en mening med båda uppgifterna.
         //Den här ordningen — ett tal direkt följt av en hel textrad — brukar bete sig oväntat. Om namnet inte blir
         //inläst: fundera på vad som låg kvar och väntade efter att talet lästes in.
+
+        Scanner input = new Scanner(System.in);
 
 
     }
