84c8251f1221f38fd11656efeaaf94b4a85ebd88
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 11:37:33 +0200

    details exercise

---
+++ src/main/java/week1/Input.java
@@ -0,0 +1,7 @@
+package week1;
+
+public class Input {
+    public static void main(String[] args) {
+        //Fråga efter användarens namn och hälsa tillbaka, med namnet inbakat i meddelandet.
+    }
+}
