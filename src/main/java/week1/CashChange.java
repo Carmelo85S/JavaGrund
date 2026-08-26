07a30ef58e5e1c13488492357f693b154221aed4
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 12:54:02 +0200

    CashChange, details commented

---
+++ src/main/java/week1/CashChange.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class CashChange {
+    public static void main(String[] args) {
+        //Fråga efter ett helt belopp i kronor och skriv ut hur många hundralappar, tiokronor och enkronor som behövs
+        //för att nå beloppet, med så stora valörer som möjligt.
+        //Körningsexempel:
+        //Belopp: 327
+        //327 kr = 3 st 100, 2 st 10, 7 st 1
+
+
+    }
+}
