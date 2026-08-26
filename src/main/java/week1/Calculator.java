0412767a0b948790e802d3f9d67d98a567bc7b3c
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 14:27:39 +0200

    working calculator

--- src/main/java/week1/Calculator.java
+++ src/main/java/week1/Calculator.java
@@ -1,7 +1,24 @@
 package week1;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insert first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Insert second number: ");
+        double b = sc.nextDouble();
+        double sum = a + b;
+        double diff = a - b;
+        double prod = a * b;
+        double kvot = a / b;
+        double rest = a % b;
+        System.out.println("ADDITION = " + sum);
+        System.out.println("DIFFERENS = " + diff);
+        System.out.println("PRODUKT = " + prod);
+        System.out.printf("KVOT = %.2f%n", kvot);
+        System.out.println("REST = " + rest);
+        sc.close();
     }
 }
