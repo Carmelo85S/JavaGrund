9b4875ba1c07e60eb5d7872541e998ab84e16d56
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 14:57:06 +0200

    switch working

--- src/main/java/week1/TempConverter.java
+++ src/main/java/week1/TempConverter.java
@@ -8,9 +8,27 @@ public class TempConverter {
 
         System.out.print("Celsius : ");
         double celsius = scanner.nextDouble();
-        double fahrenheit = (celsius * 9.0 / 5.0) + 32;
-        double kelvin = (celsius + 273.15);
-        System.out.printf("Fahrenheit: %.2f%n", fahrenheit);
-        System.out.printf("Kelvin: %.2f%n", kelvin);
+
+        System.out.println("Convert temperature");
+        System.out.println("Farenheit press F");
+        System.out.println("Kelvin press K");
+
+        System.out.print("You chosen option is: ");
+        char choose = scanner.next().charAt(0);
+        
+        switch(choose){
+            case 'F':
+                double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+                System.out.printf("Fahrenheit: %.2f%n", fahrenheit);
+                break;
+            case 'K':
+                 double kelvin = (celsius + 273.15);
+                 System.out.printf("Kelvin: %.2f%n", kelvin);
+                 break;
+            default:
+                System.out.println("Not allowed");
+                break;
+        }
+        scanner.close();
     }
 }
