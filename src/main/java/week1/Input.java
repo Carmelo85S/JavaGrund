78199a0621ab8f83b7939e4e0dcbc75df34109ce
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 11:40:41 +0200

    mini program working

--- src/main/java/week1/Input.java
+++ src/main/java/week1/Input.java
@@ -1,7 +1,14 @@
 package week1;
 
+import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
         //Fråga efter användarens namn och hälsa tillbaka, med namnet inbakat i meddelandet.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Whats your name? ");
+        System.out.print("Insert your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name +"!");
     }
 }
