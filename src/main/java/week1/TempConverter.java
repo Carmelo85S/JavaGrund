eb6c456d5468a9fbb538f40123ea4fa597fc65a2
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 15:30:48 +0200

    switch menu working

--- src/main/java/week1/TempConverter.java
+++ src/main/java/week1/TempConverter.java
@@ -6,29 +6,63 @@ public class TempConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Celsius : ");
-        double celsius = scanner.nextDouble();
+        System.out.println(" ---WELCOME TO TEMPERATURE CONVERTER!---");
+        System.out.println("|   Choose an option:                   |");
+        System.out.println("|   C = Celsius                         |");
+        System.out.println("|   F = Fahrenheit                      |");
+        System.out.println("|   K = Kelvin                          |");
+        System.out.println(" ---------------------------------------");
 
-        System.out.println("Convert temperature");
-        System.out.println("Farenheit press F");
-        System.out.println("Kelvin press K");
+        System.out.print("Option: ");
+        String tempChoice = scanner.nextLine().toUpperCase();
 
-        System.out.print("You chosen option is: ");
-        char choose = scanner.next().charAt(0);
-        
-        switch(choose){
-            case 'F':
-                double fahrenheit = (celsius * 9.0 / 5.0) + 32;
-                System.out.printf("Fahrenheit: %.2f%n", fahrenheit);
+        if(tempChoice.equals("C") || tempChoice.equals("F") || tempChoice.equals("K")) {
+
+            System.out.print("Insert temperature: ");
+            double temperature = scanner.nextDouble();
+
+            switch (tempChoice) {
+
+            case "C":
+                double fahrenheit = (temperature * 9.0 / 5.0) + 32;
+                double kelvin = temperature + 273.15;
+
+                System.out.printf("%.2f Celsius = %.2f Fahrenheit%n",
+                        temperature, fahrenheit);
+
+                System.out.printf("%.2f Celsius = %.2f Kelvin%n",
+                        temperature, kelvin);
                 break;
-            case 'K':
-                 double kelvin = (celsius + 273.15);
-                 System.out.printf("Kelvin: %.2f%n", kelvin);
-                 break;
+
+            case "F":
+                double celsius = (temperature - 32) * 5.0 / 9.0;
+                double kelvinFromF = (temperature - 32) * 5.0 / 9.0 + 273.15;
+
+                System.out.printf("%.2f Fahrenheit = %.2f Celsius%n",
+                        temperature, celsius);
+
+                System.out.printf("%.2f Fahrenheit = %.2f Kelvin%n",
+                        temperature, kelvinFromF);
+                break;
+
+            case "K":
+                double celsiusFromK = temperature - 273.15;
+                double fahrenheitFromK = (temperature - 273.15) * 9.0 / 5.0 + 32;
+
+                System.out.printf("%.2f Kelvin = %.2f Celsius%n",
+                        temperature, celsiusFromK);
+
+                System.out.printf("%.2f Kelvin = %.2f Fahrenheit%n",
+                        temperature, fahrenheitFromK);
+                break;
+
             default:
                 System.out.println("Not allowed");
-                break;
         }
+        } else {
+            System.out.println("Get out of here");
+        }
+
         scanner.close();
     }
 }
