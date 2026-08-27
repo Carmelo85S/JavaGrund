8eb6fdb25366b264c85952fc75821d5cb6e8986f
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-27 14:16:09 +0200

    new class and new exercise, commented details, import scanner

---
+++ src/main/java/week1/ChangePlace.java
@@ -0,0 +1,15 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ChangePlace {
+    public static void main(String[] args) {
+        //Fråga efter två tal och spara dem i var sin variabel. Skriv ut dem, byt sedan innehållet mellan de två
+        //variablerna så att den första får den andras värde och tvärtom, och skriv ut dem igen.
+        //Klurigt: hur byter du innehållet i två variabler utan att tappa bort det ena värdet på vägen?
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
