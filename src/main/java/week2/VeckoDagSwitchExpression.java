86bf7f623996f41f0ec37a8ae6beeecc104d5c8b
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 10:35:02 +0200

    imported class Scanner, initialize variable choice

--- src/main/java/week2/VeckoDagSwitchExpression.java
+++ src/main/java/week2/VeckoDagSwitchExpression.java
@@ -1,8 +1,17 @@
 package week2;
 
+import java.util.Scanner;
+
 public class VeckoDagSwitchExpression {
     public static void main(String[] args) {
         //Skriv ett programm som läser ett tal 1 - 7 och skriver ut veckdagen
         //med en switch expression
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which day is it?");
+        System.out.print("Choose a numer 1 - 7: ");
+
+        int choice = scanner.nextInt();
+
+
     }
 }
