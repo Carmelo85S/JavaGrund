4cbc9d4f394eecfada309a3e34bc7b02c9b4c096
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 12:34:27 +0200

    polish

--- src/main/java/week2/GuessNumber.java
+++ src/main/java/week2/GuessNumber.java
@@ -8,18 +8,22 @@ public class GuessNumber {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int secretNumber = random.nextInt(10)+1;
+        int secretNumber = random.nextInt(100)+1;
+        System.out.println("The program generate a secret number between 1 and 100.");
+        System.out.println("Your goal is to guess the right number.");
+
         int guess;
         do{
             System.out.print("Guess the secret number: ");
             guess = scanner.nextInt();
 
+            System.out.println("You guessed: "+ guess);
             if(guess < secretNumber){
-                System.out.println("You guess is less");
+                System.out.println("You guess is less than the secret number.");
             } else if(guess > secretNumber){
-                System.out.println("Your guess is high");
+                System.out.println("Your guess is higher that the secret number.");
             } else {
-                System.out.println("RIGHT");
+                System.out.println("YOU GUESSED RIGHT");
             }
 
         }  while (guess != secretNumber);
