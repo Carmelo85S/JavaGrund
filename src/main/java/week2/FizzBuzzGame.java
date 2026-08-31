b85c3e7093a503a6b34d1c4e971c9c31807071eb
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 12:04:28 +0200

    Created new class, imported scanner

---
+++ src/main/java/week2/FizzBuzzGame.java
@@ -0,0 +1,10 @@
+package week2;
+
+import java.util.Scanner;
+
+public class FizzBuzzGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
