28434ea2b9bac48803b4ed455f506314ce38682e
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 14:44:11 +0200

    commented details

--- src/main/java/week2/MediumValue.java
+++ src/main/java/week2/MediumValue.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class MediumValue {
     public static void main(String[] args) {
 
+        //Läs in tal från användaren, ett i taget, tills användaren skriver 0.
+        // När 0 matas in ska programmet sluta läsa och
+        //skriva ut hur många tal som matades in (0 räknas inte) samt deras medelvärde.
+        //Hantera fallet att det allra första talet är 0 — då finns inga tal att räkna medelvärde på.
         Scanner scanner = new Scanner(System.in);
 
         int choice;
