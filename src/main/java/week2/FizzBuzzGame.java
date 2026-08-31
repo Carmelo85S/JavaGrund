7f8bfe6f903e210694d67a5c69d971cb30738d10
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 12:12:30 +0200

    working fizz buzz program

--- src/main/java/week2/FizzBuzzGame.java
+++ src/main/java/week2/FizzBuzzGame.java
@@ -6,5 +6,18 @@ public class FizzBuzzGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Insert a number: ");
+        int number = scanner.nextInt();
+
+        if(number%3 == 0 && number%5 == 0){
+            System.out.println("Fizz Buzz!");
+        }else if(number%3 == 0){
+            System.out.println("Fizz!");
+        }else if(number%5 == 0) {
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println("Not fizz not buzz");
+        }
     }
 }
