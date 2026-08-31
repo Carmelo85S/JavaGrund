e0346b036bb43999add70be1c37b66aa37348e48
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 11:18:49 +0200

    countdown

---
+++ src/main/java/week2/CountDown.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class CountDown {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Avfyrning");
+    }
+}
