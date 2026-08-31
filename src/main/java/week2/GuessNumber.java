c8f6ace72446917cc99898a41f9551ebdf5524d6
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 12:19:02 +0200

    imported scanner, random, generated a secretNumber 1 - 100

---
+++ src/main/java/week2/GuessNumber.java
@@ -0,0 +1,15 @@
+package week2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100);
+
+    }
+
+}
