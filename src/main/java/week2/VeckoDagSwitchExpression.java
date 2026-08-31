bb2243d92979d039dd5a3d43c9a5a1c729b1de48
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 10:43:15 +0200

    working

--- src/main/java/week2/VeckoDagSwitchExpression.java
+++ src/main/java/week2/VeckoDagSwitchExpression.java
@@ -6,12 +6,20 @@ public class VeckoDagSwitchExpression {
     public static void main(String[] args) {
         //Skriv ett programm som läser ett tal 1 - 7 och skriver ut veckdagen
         //med en switch expression
+        //Utoka med ytterligare en switch expression:
+        //gruppera 1- 5 som "Vardagar" och 6 - 7 som "Helg
         Scanner scanner = new Scanner(System.in);
         System.out.println("Which day is it?");
         System.out.print("Choose a numer 1 - 7: ");
 
         int choice = scanner.nextInt();
 
+        String day = switch (choice){
+            case 1, 2, 3, 4, 5 -> "Vardagar";
+            case 6, 7 -> "Helg";
+            default ->"Invalid input";
+        };
 
+        System.out.println(day);
     }
 }
