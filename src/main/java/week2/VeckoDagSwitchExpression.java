c3d77c8fd53761b4761f05e624c43423f531902e
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 10:32:26 +0200

    .Switch expression ex, detail commented

---
+++ src/main/java/week2/VeckoDagSwitchExpression.java
@@ -0,0 +1,8 @@
+package week2;
+
+public class VeckoDagSwitchExpression {
+    public static void main(String[] args) {
+        //Skriv ett programm som läser ett tal 1 - 7 och skriver ut veckdagen
+        //med en switch expression
+    }
+}
