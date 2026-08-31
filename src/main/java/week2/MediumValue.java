44f8f907a2ed4a3326faa0032699e97c56152fb9
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 14:43:19 +0200

    polish

--- src/main/java/week2/MediumValue.java
+++ src/main/java/week2/MediumValue.java
@@ -4,20 +4,30 @@ import java.util.Scanner;
 
 public class MediumValue {
     public static void main(String[] args) {
-        //Läs in tal från användaren, ett i taget, tills användaren skriver 0.
-        // När 0 matas in ska programmet sluta läsa och
-        //skriva ut hur många tal som matades in (0 räknas inte) samt deras medelvärde.
-        //Hantera fallet att det allra första talet är 0 — då finns inga tal att räkna medelvärde på.
+
         Scanner scanner = new Scanner(System.in);
 
         int choice;
         int insert = 0;
-        int total = 0;
-        do{
-            System.out.print("Insert a number");
+        double total = 0;
+
+        do {
+            System.out.print("Insert a number: ");
             choice = scanner.nextInt();
-            insert++;
-            total = total + insert;
-        }while(choice!=0);
+
+            if (choice != 0) {
+                insert++;
+                total += choice;
+            }
+
+        } while (choice != 0);
+
+        if (insert > 0) {
+            double mediumValue = total / insert;
+            System.out.println("Numbers inserted: " + insert);
+            System.out.println("Average: " + mediumValue);
+        } else {
+            System.out.println("No numbers were entered.");
+        }
     }
 }
