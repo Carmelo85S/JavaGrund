24ca93fe32806dfff9bd1110ca0a9b21b574691d
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-31 12:29:05 +0200

    imported scanner, random, generated a secretNumber 1 - 100

--- src/main/java/week2/GuessNumber.java
+++ src/main/java/week2/GuessNumber.java
@@ -8,7 +8,20 @@ public class GuessNumber {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int secretNumber = random.nextInt(100);
+        int secretNumber = random.nextInt(10)+1;
+        int guess;
+        do{
+            System.out.print("Guess the secret number: ");
+            guess = scanner.nextInt();
+            if(guess < secretNumber){
+                System.out.println("You guess is less");
+            } else if(guess > secretNumber){
+                System.out.println("Your guess is high");
+            } else {
+                System.out.println("RIGHT");
+            }
+
+        }  while (guess != secretNumber);
 
     }
 
