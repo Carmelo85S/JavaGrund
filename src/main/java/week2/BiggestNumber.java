b14c6600cd58b656bddd4a2953d2ab1c4c657a5c
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-02 17:55:17 +0200

    biggest num method

---
+++ src/main/java/week2/BiggestNumber.java
@@ -0,0 +1,33 @@
+package week2;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        //Skriv två metoder med samma namn: en som tar två int och returnerar det största, och en som tar två double
+        //och returnerar det största. Anropa båda från main och låt Java välja rätt version automatiskt utifrån vad du
+        //skickar in.
+
+        int a = 15;
+        int b = 8;
+
+        System.out.println(compareNum(a, b));
+
+        double c = 8.4;
+        double d = 5.3;
+
+        System.out.println(compareNum(c, d));
+    }
+
+    public static int compareNum(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+
+    public static double compareNum(double a, double b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+}
