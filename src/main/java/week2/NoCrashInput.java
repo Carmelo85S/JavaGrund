5cbc298fe8c9fa69c721f4e97447cfd89595fdd3
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-03 09:55:54 +0200

    crash safe working program

--- src/main/java/week2/NoCrashInput.java
+++ src/main/java/week2/NoCrashInput.java
@@ -10,18 +10,24 @@ public class NoCrashInput {
         //som returnerar det giltiga talet.
         //Detta mönster är kursens viktigaste byggsten för krasch-säkra program. Du kommer återanvända det.
         Scanner scanner = new Scanner(System.in);
-        int myAge = userInput(scanner);
+        int a = 18;
+        int b = 99;
+        int myAge = userInput(scanner, a, b);
         System.out.println("You are "+ myAge + " years old.");
 
 
     }
-    public static int userInput(Scanner scanner){
+    public static int userInput(Scanner scanner, int min, int max){
         while(true){
             System.out.println("Enter you age");
+            System.out.println("You can choose number between "+ min +" and "+ max);
             try{
                 int age = scanner.nextInt();
                 scanner.nextLine();
-                return age;
+                if (age >= min && age <= max) {
+                    return age;
+                }
+                System.out.println("Number must be between " + min + " and " + max + ".");
             }catch(InputMismatchException e){
                 System.out.println("Not a valid input. Write a valid number...");
                 scanner.nextLine();
