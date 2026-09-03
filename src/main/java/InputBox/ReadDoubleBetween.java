ac2a3fc6d3af6e20d4e7d7e592740c264d150998
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-03 21:40:07 +0200

    chane in printf

--- src/main/java/InputBox/ReadDoubleBetween.java
+++ src/main/java/InputBox/ReadDoubleBetween.java
@@ -17,7 +17,7 @@ public class ReadDoubleBetween {
                     return value;
                 }
 
-                System.out.printf("Value should be between %d och %d%n", min, max);
+                System.out.printf("Value should be between %.2foch %.2f%n", min, max);
 
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a number.");
