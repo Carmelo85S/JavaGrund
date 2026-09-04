e21ac8e5a91c6502af910a6c75889a7347b69220
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-04 10:54:12 +0200

    user input and check

--- src/main/java/week3/CreateHashSet.java
+++ src/main/java/week3/CreateHashSet.java
@@ -1,24 +1,30 @@
 package week3;
 
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class CreateHashSet {
     public static void main(String[] args) {
         //create an HashSet string and add names
         HashSet<String> names = new HashSet<>();
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert a name: ");
+        String input = scanner.nextLine();
+
         names.add("Anna");
         names.add("Anna");
         names.add("Marco");
         names.add("Lucio");
 
-        System.out.println(names);
-
-        //check i a name is present
-        if(names.contains("Anna")){
+        if (names.contains(input)) {
             System.out.println("Already present");
+        } else {
+            System.out.println("Added");
+            names.add(input);
         }
 
-
+        System.out.println(names);
+        scanner.close();
     }
 }
