a753d5224a25ed0f2320e4fb25b7227133184035
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-04 10:42:32 +0200

    HashSet exercise

---
+++ src/main/java/week3/CreateHashSet.java
@@ -0,0 +1,11 @@
+package week3;
+
+import java.util.HashSet;
+
+public class CreateHashSet {
+    public static void main(String[] args) {
+        //create an HashSet string and add names
+        HashSet<String> names = new HashSet<>();
+
+    }
+}
