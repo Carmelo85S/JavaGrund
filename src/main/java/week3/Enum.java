0302e5787158e68a95012c34f06b3cead6fd6bf6
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-04 11:35:07 +0200

    enum ex

---
+++ src/main/java/week3/Enum.java
@@ -0,0 +1,14 @@
+package week3;
+
+public class Enum {
+    public enum Color{
+        RED,
+        GREEN,
+        BLUE
+    }
+    public static void main(String[] args) {
+        Color color = Color.RED;
+        System.out.println("Color is: "+ color);
+    }
+
+}
