834db8cbeb868586c79fb5ccf3a7adb0f04181c8
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-04 10:45:33 +0200

    HashSet exercise

--- src/main/java/week3/CreateHashSet.java
+++ src/main/java/week3/CreateHashSet.java
@@ -7,5 +7,18 @@ public class CreateHashSet {
         //create an HashSet string and add names
         HashSet<String> names = new HashSet<>();
 
+        names.add("Anna");
+        names.add("Anna");
+        names.add("Marco");
+        names.add("Lucio");
+
+        System.out.println(names);
+
+        //check i a name is present
+        if(names.contains("Anna")){
+            System.out.println("Already present");
+        }
+
+
     }
 }
