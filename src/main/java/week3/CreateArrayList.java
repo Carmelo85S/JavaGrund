1e6631d7a52210e71dc815f7c2aefc17756e126d
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-04 10:18:15 +0200

    print using foreach

--- src/main/java/week3/CreateArrayList.java
+++ src/main/java/week3/CreateArrayList.java
@@ -17,13 +17,47 @@ public class CreateArrayList {
         System.out.println("Number of element: "+names.size());
 
         //Marco -> David
+        System.out.println("------------------");
+        System.out.println("Marco -> David");
+        System.out.println("------------------");
+
         names.set(2, "David");
         System.out.println("Marco is gone, welcome to: "+ names.get(2));
 
         //print all list
+        System.out.println("print all list");
+
         for(String name : names){
             System.out.println(name);
         }
+
+        //Delete luca and print list
+        System.out.println("------------------");
+        System.out.println("Delete luca and print list");
+        System.out.println("------------------");
+
+        names.remove(1);
+        for(String name : names){
+            System.out.println(name);
+        }
+
+        //print list using for loop
+        System.out.println("------------------");
+        System.out.println("print list using for loop");
+        System.out.println("------------------");
+        for(int i = 0; i < names.size(); i++){
+            System.out.println(names.get(i));
+        }
+
+        //print list with foreach
+        System.out.println("------------------");
+        System.out.println("print list with foreach");
+        System.out.println("------------------");
+        for(String name : names){
+            System.out.println(name);
+        }
+
+
 
     }
 }
