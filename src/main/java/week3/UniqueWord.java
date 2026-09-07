c102445c99bbed758bb36dabb1c6595fd31c74ed
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-07 15:14:17 +0200

    HashSet ex

---
+++ src/main/java/week3/UniqueWord.java
@@ -0,0 +1,21 @@
+package week3;
+
+import java.util.HashSet;
+
+public class UniqueWord {
+    public static void main(String[] args) {
+        //Lägg orden "hej, hej, världen, hej, världen" i en HashSet<String>.
+        // Skriv ut hur många element setet innehåller och vilka de är.
+        // Fundera på varför antalet inte blir fem.
+
+        HashSet<String> text = new HashSet<>();
+        text.add("hej");
+        text.add("hej");
+        text.add("world");
+        text.add("hej");
+        text.add("world");
+
+        System.out.println("Size: "+text.size());
+
+    }
+}
