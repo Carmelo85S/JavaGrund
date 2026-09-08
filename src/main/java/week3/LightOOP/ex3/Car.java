834ae752e1fd4d61fe4fee2e00b011ca68cbf5a4
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-09-08 10:25:26 +0200

    working

--- src/main/java/week3/LightOOP/ex3/Car.java
+++ src/main/java/week3/LightOOP/ex3/Car.java
@@ -1,12 +1,14 @@
 package week3.LightOOP.ex3;
 
 public class Car {
+    public String name;
     public String model;
     public int year;
-    public double price;
+    public int price;
     public Engine engine;
 
-    public Car(String model, int year, double price, Engine engine){
+    public Car(String name, String model, int year, int price, Engine engine){
+        this.name = name;
         this.model = model;
         this.year = year;
         this.price = price;
--- src/main/java/week3/LightOOP/ex3/CarHasEngine.java
+++ src/main/java/week3/LightOOP/ex3/CarHasEngine.java
@@ -3,12 +3,22 @@ package week3.LightOOP.ex3;
 public class CarHasEngine {
     public static void main(String[] args) {
         // Create a Car class with the fields:
-        // model, year, price and public Engine engine.
+        // name, model, year, price and public Engine engine.
         // Add a constructor that initializes all four fields.
         //
         // In Main:
         // Create an Engine object and a Car object.
         // Print the car information, including the engine's name
         // and fuel type (car.engine.name and car.engine.fuelType).
+        Engine engine = new Engine("2.9L twin-turbo V8", 478, FuelType.PETROL);
+        Car ferrari = new Car("Ferrari","F-40", 1987, 300000, engine);
+
+        System.out.printf("Car name: %s%n",ferrari.name);
+        System.out.printf("Year of production: %d%n",ferrari.year);
+        System.out.printf("Price: %d € %n",ferrari.price);
+        System.out.printf("Engine model: %s%n",ferrari.engine.model);
+        System.out.printf("Fuel type: %s%n",ferrari.engine.fuelType);
+        System.out.printf("Horse power: %d%n",ferrari.engine.horsePower);
+
     }
 }
